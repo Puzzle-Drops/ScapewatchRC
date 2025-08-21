7df5ffb2a12b098000a13444ec667b33e5758f57
Puzzle-Drops <86485071+Puzzle-Drops@users.noreply.github.com>
2025-08-21 12:00:39 -0400

    Update skillRegistry.js

--- js/skillRegistry.js
+++ js/skillRegistry.js
@@ -17,6 +17,7 @@ class SkillRegistry {
         this.register(new SmithingSkill());
         this.register(new FletchingSkill());
         this.register(new CraftingSkill());
+        this.register(new ConstructionSkill());
         
         this.initialized = true;
         console.log('Skill registry initialized with', Object.keys(this.skills).length, 'skills');
